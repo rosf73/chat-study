ce011b11f43e8c93fb57816650570a3468d2cea1
rosf73 <alswlzhsclq@naver.com>
2022-12-17 22:08:22 +0900

    feat: define message entity

---
+++ app/src/main/java/rosf73/study/chat/data/Message.kt
@@ -0,0 +1,8 @@
+package rosf73.study.chat.data
+
+data class Message(
+    val text: String? = null,
+    val name: String? = null,
+    val photoUrl: String? = null,
+    val imageUrl: String? = null,
+)
