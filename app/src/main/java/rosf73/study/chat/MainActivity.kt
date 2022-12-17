0491b52903774f31f4b085acbaef986cf9c41a45
rosf73 <alswlzhsclq@naver.com>
2022-12-17 22:06:25 +0900

    feat: apply viewBinding in MainActivity

--- app/src/main/java/rosf73/study/chat/MainActivity.kt
+++ app/src/main/java/rosf73/study/chat/MainActivity.kt
@@ -6,14 +6,18 @@ import android.os.Bundle
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import rosf73.study.chat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         auth = Firebase.auth
         if (auth.currentUser == null) {
