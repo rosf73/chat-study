bb659e944fff8e6e94b6888787a607de40242679
rosf73 <alswlzhsclq@naver.com>
2022-12-17 16:41:18 +0900

    feat: display firebase auth ui

--- app/src/main/java/rosf73/study/chat/LoginActivity.kt
+++ app/src/main/java/rosf73/study/chat/LoginActivity.kt
@@ -1,11 +1,50 @@
 package rosf73.study.chat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    private val login: ActivityResultLauncher<Intent> =
+        registerForActivityResult(FirebaseAuthUIActivityResultContract(), this::onLoginResult)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth = Firebase.auth
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if (Firebase.auth.currentUser == null) {
+            val loginIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setLogo(R.drawable.horseballoon)
+                .setAvailableProviders(listOf(
+                    AuthUI.IdpConfig.EmailBuilder().build(),
+                    AuthUI.IdpConfig.GoogleBuilder().build(),
+                ))
+                .build()
+
+            login.launch(loginIntent)
+        } else {
+
+        }
+    }
+
+    private fun onLoginResult(result: FirebaseAuthUIAuthenticationResult) {
+
     }
 }
