7de8c9ffa1edc6d40abdd43d0448760072015c36
rosf73 <alswlzhsclq@naver.com>
2022-12-17 16:53:35 +0900

    feat: write login result function

--- app/src/main/java/rosf73/study/chat/LoginActivity.kt
+++ app/src/main/java/rosf73/study/chat/LoginActivity.kt
@@ -3,6 +3,7 @@ package rosf73.study.chat
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
@@ -40,11 +41,30 @@ class LoginActivity : AppCompatActivity() {
 
             login.launch(loginIntent)
         } else {
-
+            navigateToMain()
         }
     }
 
     private fun onLoginResult(result: FirebaseAuthUIAuthenticationResult) {
+        if (result.resultCode == RESULT_OK) {
+            navigateToMain()
+        } else {
+            Toast.makeText(
+                this,
+                "There was an error signing in",
+                Toast.LENGTH_LONG).show()
 
+            val response = result.idpResponse
+            if (response == null) {
+                println("Sign in canceled")
+            } else {
+                println("Sign in error :: ${response.error}")
+            }
+        }
+    }
+
+    private fun navigateToMain() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
