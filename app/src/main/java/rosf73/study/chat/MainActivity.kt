8933600889b8ca55601a30b451cadc03a13c2a1a
rosf73 <alswlzhsclq@naver.com>
2022-12-18 18:15:25 +0900

    feat: open document for image upload - connect to firebase storage - database setValue google loading image

--- app/src/main/java/rosf73/study/chat/MainActivity.kt
+++ app/src/main/java/rosf73/study/chat/MainActivity.kt
@@ -1,6 +1,7 @@
 package rosf73.study.chat
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ProgressBar
@@ -8,11 +9,16 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.ktx.storage
 import rosf73.study.chat.data.Message
 import rosf73.study.chat.databinding.ActivityMainBinding
+import rosf73.study.chat.util.MyLinearLayoutManager
+import rosf73.study.chat.util.OpenDocumentContract
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,6 +29,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var db: FirebaseDatabase
 
     private lateinit var adapter: MessageAdapter
+
+    private val openDocument = registerForActivityResult(OpenDocumentContract()) { uri ->
+        uri?.let { onImageSelected(it) }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -68,7 +78,7 @@ class MainActivity : AppCompatActivity() {
             .build()
 
         adapter = MessageAdapter(options, getUserName())
-        manager = LinearLayoutManager(this)
+        manager = MyLinearLayoutManager(this)
         manager.stackFromEnd = true
 
         binding.pbMain.visibility = ProgressBar.INVISIBLE
@@ -108,10 +118,57 @@ class MainActivity : AppCompatActivity() {
             db.reference.child(MESSAGES_CHILD).push().setValue(msg) // send message (no image)
             binding.etMessage.setText("")
         }
+
+        binding.btnAdd.setOnClickListener {
+            // intent device document
+            openDocument.launch(arrayOf("image/*"))
+        }
+    }
+
+    private fun onImageSelected(uri: Uri) {
+        val user = auth.currentUser
+        val tempMessage = Message(null, getUserName(), getPhotoUrl(), LOADING_IMAGE_URL)
+        db.reference
+            .child(MESSAGES_CHILD)
+            .push()
+            .setValue(
+                tempMessage,
+                DatabaseReference.CompletionListener { databaseError, databaseReference ->
+                    if (databaseError != null) {
+                        return@CompletionListener
+                    }
+
+                    // upload the image file
+                    val key = databaseReference.key
+                    val storageReference = Firebase.storage
+                        .getReference(user!!.uid)
+                        .child(key!!)
+                        .child(uri.lastPathSegment!!)
+                    putImageInStorage(storageReference, uri, key)
+                })
+    }
+
+    private fun putImageInStorage(storageReference: StorageReference, uri: Uri, key: String?) {
+        storageReference.putFile(uri)
+            .addOnSuccessListener(
+                this
+            ) { taskSnapshot ->
+                taskSnapshot.metadata!!.reference!!.downloadUrl
+                    .addOnSuccessListener { uri ->
+                        // put downloadUrl in database
+                        val msg = Message(null, getUserName(), getPhotoUrl(), uri.toString())
+                        db.reference
+                            .child(MESSAGES_CHILD)
+                            .child(key!!)
+                            .setValue(msg)
+                    }
+            }
+            .addOnFailureListener(this) { e -> }
     }
 
     companion object {
         const val MESSAGES_CHILD = "messages"
         const val ANONYMOUS = "anonymous"
+        private const val LOADING_IMAGE_URL = "https://www.google.com/images/spin-32.gif"
     }
 }
